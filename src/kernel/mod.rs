aca0a79a2718a2241dc0a98add1c41efa02ae95a
Yokimt <498588005@qq.com>
2025-07-21 21:36:40 +0800

    init

---
+++ src/kernel/mod.rs
@@ -0,0 +1,113 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod sukisu;
+
+use std::os::raw::{c_char, c_int, c_long, c_void};
+use syscalls::{syscall, Sysno};
+#[repr(C)]
+pub struct kpm_read {
+    pub key: u16,
+    pub pid: c_int,
+    pub size: c_int,
+    pub addr: u64,
+    pub buffer: *mut c_void,
+}
+#[repr(C)]
+pub struct kpm_mod {
+    pub key: u16,
+    pub pid: c_int,
+    pub SoName: [c_char; 256],
+    pub base: u64,
+    pub pkg_name: [c_char; 256],
+}
+
+pub struct KernelDriver {
+    kread: kpm_read,
+    kmod: kpm_mod,
+    cmd_read: u16,
+    cmd_write: u16,
+    cmd_mod: u16,
+    cmd_pid: u16,
+}
+impl KernelDriver {
+    pub fn new() -> Self {
+        KernelDriver {
+            kread: kpm_read {
+                key: 0,
+                pid: 0,
+                size: 0,
+                addr: 0,
+                buffer: std::ptr::null_mut(),
+            },
+            kmod: kpm_mod {
+                key: 0,
+                pid: 0,
+                SoName: [0; 256],
+                base: 0,
+                pkg_name: [0; 256],
+            },
+            cmd_read: 0,
+            cmd_write: 0,
+            cmd_mod: 0,
+            cmd_pid: 0,
+        }
+    }
+    // 初始化命令控制
+    pub fn cmd_ctl(&mut self) -> i32 {
+        let kpm_name = "kernel-mem";
+        let ret = sukisu::kpm_control(kpm_name, "get_key");
+
+        if ret < 0 {
+            return ret;
+        }
+        self.kread.key = (ret & 0xFFFF) as u16;
+        self.cmd_read = ((ret >> 16) & 0xFFFF) as u16;
+        self.init(self.cmd_read, self.kread.key);
+        0
+    }
+    pub fn init(&mut self,cmd:u16,key:u16)
+    {
+        self.cmd_read = cmd;
+        self.cmd_write = cmd + 1;
+        self.cmd_mod = cmd + 2;
+        self.cmd_pid = cmd + 3;
+        self.kread.key = key;
+        self.kmod.key = key;
+    }
+    pub fn readmem(&mut self,addr:u64,buffer: *mut c_void,size:c_int)->i32
+    {
+        self.kread.addr = addr;
+        self.kread.buffer = buffer;
+        self.kread.size = size;
+        let result = unsafe {
+         syscall!(
+            Sysno::ioctl,
+            self.cmd_read as c_int,
+            &self.kread as *const kpm_read as usize
+            )
+        };
+        match result {
+            Ok(ret) => {
+                if ret > 0 {
+                    println!("read Success");
+                }
+                ret as i32
+            }
+            Err(err) => {
+                eprintln!("read failed: {}", err);
+                -25
+            }
+        }
+    }
+    pub fn read<T>(&mut self,addr:u64)->T
+    {
+        self.kread.addr = addr;
+        self.kread.buffer = vec![0u8; std::mem::size_of::<T>()].as_mut_ptr() as *mut c_void;
+        self.kread.size = std::mem::size_of::<T>() as c_int;
+        self.readmem(self.kread.addr, self.kread.buffer, self.kread.size);
+        unsafe {
+            std::ptr::read(self.kread.buffer as *const T)
+        }
+    }
+}
---
+++ src/kernel/sukisu/mod.rs
@@ -0,0 +1,115 @@
+use syscalls::{syscall, Sysno};
+use std::os::raw::{c_int,c_long};
+use std::ffi::CString;
+
+const  KERNEL_SU_OPTION: c_long = 0xDEADBEEF;
+
+const KSU_OPTIONS: c_int = 0x00000001;
+// KPM控制代码
+const CMD_KPM_CONTROL:c_int =  28;
+
+const CMD_KPM_CONTROL_MAX:c_int  = 7;
+
+const SUKISU_KPM_LOAD : c_int = 28;
+
+const SUKISU_KPM_UNLOAD : c_int = 29;
+
+const SUKISU_KPM_NUM :c_int = 30;
+
+const SUKISU_KPM_LIST :c_int = 31;
+
+const SUKISU_KPM_INFO :c_int = 32;
+
+const SUKISU_KPM_CONTROL :c_int = 33;
+
+const SUKISU_KPM_VERSION :c_int = 34;
+
+pub fn load_kpm(path:&str) -> i32
+{
+    let c_path = CString::new(path).expect("Invalid load_path string (contains null byte)");
+    let mut out:c_int = -1;
+    let result =unsafe {
+        syscall!(
+            Sysno::prctl, // 系统调用号
+            KSU_OPTIONS,  // 选项
+            SUKISU_KPM_LOAD, // 子命令
+            c_path.as_ptr(), // 路径指针
+            0, // 第四个参数 (NULL)
+            &mut out as *mut c_int// 输出参数指针
+        )
+    };
+
+    match result {
+        Ok(ret) => {
+            // 检查操作结果
+            if out > 0 {
+                println!("load_kpm Success");
+            }
+            ret as i32
+        }
+        Err(err) => {
+            // 系统调用失败
+            eprintln!("load_kpm failed: {}", err);
+            -22
+        }
+    }
+}
+
+pub fn unload_kpm(path:&str) -> i32
+{
+    let c_path = CString::new(path).expect("Invalid unload_path string (contains null byte)");
+    let mut out:c_int = -1;
+    let result =unsafe {
+        syscall!(
+            Sysno::prctl, // 系统调用号
+            KSU_OPTIONS,  // 选项
+            SUKISU_KPM_UNLOAD, // 子命令
+            c_path.as_ptr(), // 路径指针
+            0, // 第四个参数 (NULL)
+            &mut out as *mut c_int // 输出参数指针
+        )
+    };
+
+    match result {
+        Ok(ret) => {
+            // 检查操作结果
+            if out > 0 {
+                println!("unload_kpm Success");
+            }
+            ret as i32
+        }
+        Err(err) => {
+            // 系统调用失败
+            eprintln!("unload_kpm failed: {}", err);
+            -23
+        }
+    }
+}
+pub fn kpm_control(name :&str, arg:&str) -> i32 {
+    let c_name = CString::new(name).expect("Invalid name string (contains null byte)");
+    let c_arg =  CString::new(arg).expect("Invalid arg string (contains null byte)");
+    let mut out: c_int = -1;
+    let result = unsafe {
+        syscall!(
+            Sysno::prctl, // 系统调用号
+            KSU_OPTIONS,  // 选项
+            CMD_KPM_CONTROL, // 控制命令
+            c_name.as_ptr(), // 命令参数
+            c_arg.as_ptr(), // 附加参数
+            &mut out as *mut c_int // 输出参数指针
+        )
+    };
+
+    match result {
+        Ok(ret) => {
+            if out > 0 {
+                println!("kpm_control Success");
+            }
+            ret as i32
+        }
+        Err(err) => {
+            eprintln!("kpm_control failed: {}", err);
+            -24
+        }
+    }
+}
